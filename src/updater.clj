d9078ec615e34475cfcb0c0d7a0aa981e0fafba1
qrt <khconsultancy@gmail.com>
2020-11-01 20:36:28 +0100

    Implement update-references command

--- src/updater.clj
+++ src/updater.clj
@@ -4,16 +4,10 @@
             [clj-http.client :as http-client]))
 
 (defn parse-tag
-  "Takes a tag of the following form
+  "Tries to parse a tag into an optional prefix string, major, minor and
+  patch integers, and an optional -rc integer.
 
-    /PREFIX//MAJOR/./MINOR/./PATCH//-rcRC/
-
-  The prefix can be an empty string and the last part starting with
-  -rc is optional. Returns a map containing the original unparsed-tag,
-  and the parts described. Prefix is kept as is, while the other parts
-  are parsed to integers.
-
-  Examples of tags:
+  Some examples to explain:
 
   v2.13.3   ;; no rc value
   => {:unparsed-tag \"v2.13.3\"
@@ -22,6 +16,11 @@
       :minor 13
       :patch 3}
 
+  v1.0 ;; no patch nor rc value
+  => {:unparsed-tag \"v1.0\"
+      :prefix \"v\"
+      :major 1
+      :minor 0}
 
   v12.1.33-rc14
   => {:unparsed-tag \"v12.1.33-rc14\"
@@ -32,37 +31,71 @@
       :rc 14}
   "
   [tag]
-  (let [nth-int #(Integer/parseInt (nth %1 %2))
-        re-prefix-major-minor-patch-rc #"([^\d]*)(\d+)\.(\d+)\.(\d+)(-rc\d+)?"
-        parts (re-find re-prefix-major-minor-patch-rc tag)
-        parsed {:unparsed-tag tag
-                :prefix (nth parts 1)
-                :major (nth-int parts 2)
-                :minor (nth-int parts 3)
-                :patch (nth-int parts 4)}]
-    (conj parsed (when-let [rc-str (nth parts 5)]
-                   [:rc (nth-int (re-find #"-rc(\d+)" rc-str) 1)]))))
+  (let [part->idx {:major 2
+                   :minor 3
+                   :patch 4}
 
-(defn find-module-sources
+        re-prefix-major-minor-patch-rc
+        #"([^\d]*)(\d+)?\.?(\d+)?\.?(\d+)?(-rc\d+)?"
+
+        parts (re-find re-prefix-major-minor-patch-rc tag)
+
+        without-rc (reduce
+                     (fn [acc [key idx]]
+                       (if-let [v-str (nth parts idx)]
+                         (assoc acc key (Integer/parseInt v-str))
+                         acc))
+                     {:unparsed-tag tag
+                      :prefix (nth parts 1)}
+                     part->idx)]
+
+    (conj without-rc (when-let [rc-str (nth parts 5)]
+                       [:rc (Integer/parseInt
+                              (nth (re-find #"-rc(\d+)" rc-str)
+                                   1))]))))
+
+(defn find-terraform-files-with-module-references
   [dir]
   (let [terraform-files
         (->> (file-seq (clojure.java.io/file dir))
-             (filter #(and (not (str/includes? (.getPath %) ".terraform"))
-                           (not (str/includes? (.getPath %) ".git"))
-                           (str/ends-with? (.getName %) ".tf"))))
+             (map #(.getPath %))
+             (filter #(and (not (str/includes? % ".terraform"))
+                           (not (str/includes? % ".git"))
+                           (str/ends-with? % ".tf"))))
 
-        github-refs (->> terraform-files
-                         (mapcat #(re-seq #"\"([^\"]*github[^\"]*)\"" (slurp %)))
-                         (map second)
-                         set
-                         sort
-                         (map (fn [url]
-                                {:url url
-                                 :module-github-url (subs url 0 (str/index-of url "?"))
-                                 :version (mapv #(Integer/parseInt %) (rest (re-find #"v(\d+)\.(\d+)\.(\d+)" url)))})))]
-    github-refs))
+        file->modules (->> terraform-files
+                           (map (fn [terraform-file]
+                                  (let [re-github-within-parenthesis
+                                        #"\"([^\"]*github[^\"]*)\""
 
-(defn get-available-tag-versions
+                                        find-github-urls
+                                        #(->> %
+                                              (re-seq re-github-within-parenthesis)
+                                              (map second)
+                                              set)]
+                                    [terraform-file
+                                     (let [contents (slurp terraform-file)]
+                                       {:contents
+                                        contents
+
+                                        :referenced-modules
+                                        (->> contents
+                                             find-github-urls
+                                             (map (fn [github-url]
+                                                    (let [re-account-repository-reference
+                                                          #"github.com:([^/]+)/([^.?]+).*\?ref=(.*)"
+
+                                                          parts
+                                                          (re-find re-account-repository-reference github-url)]
+                                                      {:original-github-url github-url
+                                                       :account (nth parts 1)
+                                                       :repository (nth parts 2)
+                                                       :tag (parse-tag (nth parts 3))})))
+                                             set)})])))
+                           (into {}))]
+    (into {} (filter #(seq (:referenced-modules (val %))) file->modules))))
+
+(defn find-available-module-tags
   [{:keys [account repository oauth-token] :or {oauth-token (System/getenv "GITHUB_TOKEN_ICE")}}]
   (let [nth-int #(Integer/parseInt (nth %1 %2))
         tags (-> (str "https://api.github.com/repos/" account "/" repository "/git/refs/tags")
@@ -70,56 +103,36 @@
                    (when oauth-token
                      {:headers {"Authorization" (str "token " oauth-token)}}))
                  :body
-                 (json/parse-string keyword))
+                 (json/parse-string keyword))]
+    (mapv #(parse-tag (subs (:ref %) (count "refs/tags/")))
+          tags)))
 
-        parsed-tags
-        (map (fn [tag]
-               (->> (:ref tag)
-                    (re-find #"refs/tags/([^\d]*)(\d+)\.(\d+)\.(\d+)(-rc\d+)?")
-                    (#(hash-map :tag (nth (re-find #"refs/tags/(.*)" (nth % 0)) 1)
-                                :prefix (nth % 1)
-                                :major (nth-int % 2)
-                                :minor (nth-int % 3)
-                                :patch (nth-int % 4)
-                                :rc (if-let [rc-str (nth % 5)]
-                                      (nth-int (re-find #"-rc(\d+)" rc-str) 1))))))
-             tags)]
+(defn update-references
+  [dir]
+  (let [->module-id
+        #(select-keys % [:account :repository])
 
-    parsed-tags))
+        file-path->contents-and-module-refs
+        (find-terraform-files-with-module-references dir)
 
-(comment
-  ;; a nicely sorted table of tag information
-  (pprint/print-table
-    [:tag :prefix :major :minor :patch :rc]
-    (reverse
-      (sort-by (juxt :major :minor :patch #(or (:rc %) "0"))
-               (get-available-tag-versions
-                 {:account "skm-ice"
-                  :repository "terraform-stack-team-iceout"}))))
+        module-id->available-tags
+        (->> (vals file-path->contents-and-module-refs)
+               (map :referenced-modules)
+               (apply concat)
+               set
+               (map (fn [module] [(->module-id module) (find-available-module-tags module)]))
+               (into {}))]
 
-  ;; a shell script
-  ;; curl -H "Authorization: token OAUTH-TOKEN" https://api.github.com/repos/skm-ice/terraform-stack-team-iceout/git/refs/tags
-
-  ;; found github.url is parsed into
-  ;; - account
-  ;; - repository
-  ;; - current version
-  ;;   - prefix
-  ;;   - main-version
-  ;;   - minor version
-  ;;   - patch version
-
-
-  ;; account and repository are resolved to
-  ;; - list of available versions each having
-  ;;   - prefix
-  ;;   - main-version
-  ;;   - minor version
-  ;;   - patch version
-
-
-  #_(require '[clojure.java.shell :as shell])
-  #_(shell/sh "pwd")
-
-
-  )
+    (doseq [[file-path contents-and-module-refs] file-path->contents-and-module-refs]
+      (let [updated-contents (reduce
+                               (fn [acc module]
+                                 (str/replace acc
+                                              (:original-github-url module)
+                                              (str/replace (:original-github-url module)
+                                                           (:unparsed-tag (:tag module))
+                                                           (:unparsed-tag (last (get module-id->available-tags
+                                                                                     (->module-id module)))))))
+                               (:contents contents-and-module-refs)
+                               (:referenced-modules contents-and-module-refs))]
+        (when (not= (:contents contents-and-module-refs) updated-contents)
+          (spit file-path updated-contents))))))
